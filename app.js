230d8038d904f0fbfffb138f576786a2691082c4
kabps3309 <kabps3309@gmail.com>
2026-05-01 15:34:20 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbz5KSoaxZcPcPcYx9ruW13vHxIpxbwk20vhbrQWA-Dm-vt30I4B6H80MI5spgopDh8S/exec";
+
+async function loadData() {
+  const res = await fetch(API_URL + "?action=get");
+  const data = await res.json();
+
+  const tbody = document.querySelector("#table tbody");
+  tbody.innerHTML = "";
+
+  data.forEach(row => {
+    const tr = `
+      <tr>
+        <td>${row.tanggal}</td>
+        <td>${row.jam}</td>
+        <td>${row.kegiatan}</td>
+        <td>${row.peserta}</td>
+        <td>${row.lokasi}</td>
+      </tr>
+    `;
+    tbody.innerHTML += tr;
+  });
+}
