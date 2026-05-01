4ec020e52db38bd59baa022f00a77bae598e1e37
kabps3309 <kabps3309@gmail.com>
2026-05-01 16:51:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbz5KSoaxZcPcPcYx9ruW13vHxIpxbwk20vhbrQWA-Dm-vt30I4B6H80MI5spgopDh8S/exec";
 
 async function loadData() {
-  const res = await fetch(API_URL + "?action=api_get");
+  const res = await fetch(API_URL + "?action=api_get"); alert(res.stringify());
   const data = await res.json();
   alert(data);
   const tbody = document.querySelector("#table tbody");
